d40f8116edf3c40f3d687cc21341032c632b9f9d
TheNewCivilian <git@prenninger.de>
2026-02-18 18:22:51 +0100

    add availability and deckvisualization component

--- src/components/WagonVisualization.vue
+++ src/components/WagonVisualization.vue
@@ -5,23 +5,17 @@
         <input id="scale-slider" type="range" min="10" max="50" v-model.number="scale" />
       </div>
       <div style="display: flex; flex-direction: row; overflow-x: auto; gap: 20px; padding: 20px;">
-          <div v-for="(deckPlan, index) in deckPlanArray" :key="`deckplan-${index}`">
+          <div v-for="(deckPlan, index) in deckPlans" :key="`deckplan-${index}`">
             <h3>{{ deckPlan.attr_id }}</h3>
             <div v-for="(deck, dIndex) in deckPlan.decks" :key="`deck-${dIndex}`">
               <h4 @click="$emit('select', { element: deck, ctrlKey: false })" :class="`${selectedElements.includes(deck) ? 'text-ott-accent' : ''} cursor-pointer`">{{ deck.Name }}</h4>
-              <v-stage
-                :config="getStageSize(deck, scale)"
-              >
-                <v-layer>
-                  <DeckVisualization 
-                    :deck="deck" 
-                    :scale="scale" 
+                  <DeckVisualization
+                    :deck="deck"
+                    :scale="scale"
                     :selectedElements="selectedElements"
                     @select="(payload) => $emit('select', payload)"
                     @area-select="(elements) => $emit('area-select', elements)"
                   />
-                </v-layer>
-              </v-stage>
             </div>
           </div>
       </div>
@@ -29,75 +23,23 @@
 </template>
 
 <script lang="ts" setup>
-import { DeckPlan } from '@/types/deckPlan';
+import { DeckPlan } from '@/types/netex/deckPlan';
 import type { PropType } from 'vue';
 import DeckVisualization from './DeckVisualization.vue';
-import { ref, watch } from 'vue';
-import type { Deck } from '@/types/deck';
-import { XMLParser } from 'fast-xml-parser';
-import { extractElementList } from '@/types/general';
-
-// Waiting for https://github.com/konvajs/vue-konva/pull/266
-// import {
-//   Stage,
-//   Layer,
-// } from 'vue-konva';
+import { ref } from 'vue';
 
 const scale = ref(20)
 
-const props = defineProps({
+defineProps({
   deckPlans: {
     type: Array as PropType<DeckPlan[]>,
     required: false,
   },
-   
+
   selectedElements: {
     type: Array as PropType<any[]>,
     default: () => [],
   }
 })
-
-const getStageSize = (deck: Deck, scale: number) => {
-  let maxWidth = 0
-  let maxHeight = 0
-
-  const { width, height } = deck.getBoundingBox()
-  maxWidth = Math.max(maxWidth, width)
-  maxHeight = Math.max(maxHeight, height)
-
-  return {
-    width: (maxWidth * scale) + 10,
-    height: (maxHeight * scale) + 10
-  }
-}
-
-const deckPlanArray = ref<DeckPlan[]>([])
-
-watch(
-  () => props.deckPlans,
-  (newValue) => {
-    if (newValue && newValue.length) {
-      deckPlanArray.value = newValue
-    }
-  },
-  { immediate: true }
-)
-
-function loadXml(xml: string) {
-  const parser = new XMLParser({
-    ignoreAttributes: false,
-    attributeNamePrefix: 'attr_',
-    removeNSPrefix: true,
-  })
-
-  const delivery = parser.parse(xml)
-
-  deckPlanArray.value = extractElementList(
-    delivery.PublicationDelivery.dataObjects.CompositeFrame.frames.ResourceFrame.deckPlans.DeckPlan,
-    DeckPlan
-  )
-}
-
-defineExpose({ loadXml })
 
 </script>
